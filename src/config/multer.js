b0b701361191faa05d31d841e6ca4db66edfb364
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-05-29 17:22:20 -0300

    rename past public

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,7 +3,7 @@ const { extname, resolve } = require("path");
 
 module.exports = {
   storage: multer.diskStorage({
-    destination: resolve(__dirname, "..", "..", "public", "uploads"),
+    destination: resolve(__dirname, "..", "..", "uploads"),
     filename: (req, file, callback) => {
       const timestamp = Date.now();
       callback(null, `${timestamp}${extname(file.originalname)}`);
