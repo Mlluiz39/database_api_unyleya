1d0b9a28c9dd7a18a80726ab8d94e49f3b807217
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-05-29 20:13:00 -0300

    add new router

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,20 @@ const multer = require('multer')
 const multerConfig = require('./config/multer')
 
 const ProductController = require('./app/controllers/ProductController')
+const Product = require('./app/models/Product')
 
 const upload = multer(multerConfig)
 
 routes.get('/', (req, res) => {
   res.json({ message: 'API de produtos da unyleya' })
+})
+
+routes.post('/product', async (req, res) => {
+  const { name, code, category, description, price, path } = req.body
+
+  const product = await Product.create({ name, code, category, description, price, path })
+
+  return res.json(product)
 })
 routes.get('/products', ProductController.index)
 routes.get('/products/:id', ProductController.show)
