a2bca841b3e9f26daf0405e7876adb80bf45c397
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-05-29 20:36:04 -0300

    add env

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,10 @@ require('dotenv/config')
 
 module.exports = {
   dialect: 'postgres',
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host:     '164.152.48.202',    
+  username: 'postgres',
+  password: 'Julia2912',
+  database: 'apiuni',
   define: {
     timestamps: true,
     underscored: true,
